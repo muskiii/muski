5eec1958b6466b2cd45bc31c0bd065312abe3c53
aspen <jmf@grayshirts.com.ar>
2017-09-29 20:01:07 -0300

    pm2 ecosystem modified

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   apps: [{
     name: 'muski',
-    script:  'server.js'
+    script: 'server.js',
+    env: {
+      COMMON_VARIABLE: 'true'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    }
   }],
   deploy: {
     production: {
@@ -12,6 +18,18 @@ module.exports = {
       repo: 'git@github.com:muskiii/muski.git',
       path: '/home/ubuntu/development/muski',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
-    }
+    },
+    dev : {
+      user : 'ubuntu',
+      host : '52.43.118.42',
+      key: '~/.ssh/AWSmuskiprivatekey.pem',
+      ref: 'origin/development',
+      repo : 'git@github.com:muskiii/muski.git',
+      path : '/home/ubuntu/production/muski',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+
   }
 }
