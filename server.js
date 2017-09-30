3f00e4ee965feb833797f379c924efdd5e8338ef
aspen <jmf@grayshirts.com.ar>
2017-09-29 22:33:39 -0300

    landing for prod and dev added and some env logic

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express'),
   mongoose = require('mongoose'),
   Grocery = require('./api/models/groceries/grocery'), //created model loading here
   bodyParser = require('body-parser');
+  var path = require('path');
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
@@ -12,12 +13,29 @@ mongoose.connect('mongodb://muski:pa55word@ds149324.mlab.com:49324/muski');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var routes = require('./api/routes/groceries/groceries'); //importing route
-routes(app); //register the route
+app.use('/static',express.static('public'));
+
+if(process.env.NODE_ENV=="dev"){
+  app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/DevIndex.html'));
+});
+}
+
+if(process.env.NODE_ENV=="production"){
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/ProdIndex.html'));
+});
+}
+
+//importing route
+var routes = require('./api/routes/groceries/groceries');
+//register the route
+routes(app); 
+//filter 404
 app.use(function(req, res){
   res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
 app.listen(port);
-
+console.log("ENV: "+process.env.NODE_ENV);
 console.log('muski Rest API server started on:' + port);
