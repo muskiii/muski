da819bd248925c9b087cb888cfe33eed75a3d4c2
aspengs <jmf@grayshirts.com.ar>
2019-04-18 13:08:19 -0300

    chum churrum

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ if(isProduction){
 }
 
 require('./models/User');
+require('./models/Entity');
 require('./config/passport');
 app.use(require('./routes'));
 app.get('*', (req,res) =>{
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,4 @@
-var mongoose = require("mongoose");
 var router = require("express").Router();
-var passport = require("passport");
-var User = mongoose.model("User");
 var auth = require("../auth");
 var controller = require("../../controllers/users");
 
