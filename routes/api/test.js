5398f233173427d6b9ca866bb7af487b05805efe
aspengs <jmf@grayshirts.com.ar>
2019-05-28 20:45:14 -0300

    added test template url

--- routes/api/test.js
+++ routes/api/test.js
@@ -1,9 +1,11 @@
 var router = require("express").Router();
 const NodeCache = require("node-cache");
+const path = require("path");
 const myCache = new NodeCache();
+const fs = require("fs");
 const key = "__express__";
 
-var cacheMiddleware = duration => {
+var cacheEveryRequest = duration => {
   return (req, res, next) => {
     let cacheContent = myCache.get(key);
     if (cacheContent == null) {
@@ -17,9 +19,40 @@ var cacheMiddleware = duration => {
 router.delete("/", function(req, res, next) {
   return res.json(myCache.set(key, [], 60 * 1000));
 });
+
 router.get("/", function(req, res, next) {
   return res.json(myCache.get(key));
 });
-router.post("/", cacheMiddleware(60));
+
+// router.get("/panic", function(req, res, next) {
+//   let testArray = myCache.get(key);
+//   let lastPosition = testArray[testArray.length -1];
+//   return res.json(lastPosition);
+// });
+
+router.get("/panic", function(req, res, next) {
+  let testArray = myCache.get(key);
+  let lastPosition = testArray[testArray.length - 1];
+
+  let data = fs.readFileSync(
+    __dirname + "/../../public/testViewTemplate.html",
+    "utf-8"
+  );
+
+  let newIndex = data.replace("/*!*long*/", parseFloat(lastPosition.longitude));
+  newIndex = newIndex.replace("/*!*lat*/", parseFloat(lastPosition.latitude));
+
+  fs.unlink(__dirname + "/../../public/testView.html", err => {
+    if (err) console.log(__dirname + "/../../public/testView.html");
+    fs.writeFileSync(
+      __dirname + "/../../public/testView.html",
+      newIndex,
+      "utf-8"
+    );
+    res.sendFile(path.join(__dirname + "/../../public/testView.html"));
+  });
+});
+
+router.post("/", cacheEveryRequest(60));
 
 module.exports = router;
